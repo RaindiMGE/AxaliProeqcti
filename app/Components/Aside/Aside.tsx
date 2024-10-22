4c368bb5771035e17d1e393b00031718efbf6d69
Anri Dumbadze <147245202+AnriDumbadze@users.noreply.github.com>
2024-10-22 21:02:43 +0400

    sec page
    sec page

--- app/Components/Aside/Aside.tsx
+++ app/Components/Aside/Aside.tsx
@@ -1,21 +1,29 @@
 "use client";
+
 import React, { useState, useEffect } from 'react';
 import styles from './Aside.module.scss';
-import { useRouter } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';  
 import Icon from '../Icon/Icon';
 import MenuItem from '../MenuItem/MenuItem';
-
-
 
 const AsideMenu = () => {
   const [activeItem, setActiveItem] = useState<string | null>(null);
   const router = useRouter();
+  const pathname = usePathname(); 
+  useEffect(() => {
+    if (pathname === '/') {
+      setActiveItem('home');
+    } else if (pathname.includes('/aboutPage')) {
+      setActiveItem('about');
+    } else if (pathname.includes('/technologyPage')) {
+      setActiveItem('technology');
+    }
+  }, [pathname]);  
 
   const handleMenuItemClick = (name: string, route: string) => {
     setActiveItem(name);
     router.replace(route);
   };
-
 
   return (
     <div className={styles.aside}>
@@ -39,13 +47,8 @@ const AsideMenu = () => {
             isActive={activeItem === "technology"}
             onClick={() => handleMenuItemClick("technology", "/technologyPage")}
           />
-
-
-
         </div>
       </div>
-
-
     </div>
   );
 };
