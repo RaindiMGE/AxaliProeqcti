186945528e42b2dca889b1c2f22b41419b40db24
RaindiMGE <mgeladze.raindi06@gmail.com>
2024-10-22 16:24:20 +0400

    refactor: Add Role For Teammate

--- app/Components/TeammateBox/TeammateBox.tsx
+++ app/Components/TeammateBox/TeammateBox.tsx
@@ -5,15 +5,20 @@ import styles from './TeammateBox.module.scss';
 interface Props {
     name: string;
     image: string;
-
+    role: string;
 }
 
 const TeammateBox = (props: Props) => {
     return <div className={styles.container}>
         <Image className={styles.image} src={props.image} alt={'teamate image'} width={142} height={142} />
-        <span className={styles.title}>
-            {props.name}
-        </span>
+        <div className={styles.description}>
+            <span className={styles.title}>
+                {props.name}
+            </span>
+            <span className={styles.role}>
+                {props.role}
+            </span>
+        </div>
     </div>
 }
 
