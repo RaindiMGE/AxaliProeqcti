b186eb174d68565a6e653a2128fe74cba0358bc0
Anri Dumbadze <147245202+AnriDumbadze@users.noreply.github.com>
2024-10-22 16:27:02 +0400

    asdasdas
    asdasdsa

--- app/Components/MenuItem/MenuItem.tsx
+++ app/Components/MenuItem/MenuItem.tsx
@@ -1,6 +1,6 @@
 "use client";
-import React from "react";
-import styles from "../MenuItem/MenuItem.module.scss"
+import React, { useEffect, useState } from "react";
+import styles from "./MenuItem.module.scss";
 import Icon from "../Icon/Icon";
 
 type Props = {
@@ -12,19 +12,20 @@ type Props = {
 const MenuItem = ({ name, isActive, onClick }: Props) => {
 
   
-  return (<div
-    className={styles.menuItem}
-    onClick={onClick}
-  >
-    <div className={styles.itemContent}>
-      <Icon name={name} isActive={isActive} onClick={onClick} />
-      <span
-        className={styles.menuText}
-      >
-        {name}
-      </span>
+  return (
+    <div
+      className={`${styles.menuItem} ${isActive ? styles.active : ""}`}
+      onClick={onClick}
+    >
+      <div className={styles.itemContent}>
+        <Icon name={name} isActive={isActive} onClick={onClick} />
+        <span
+          className={`${styles.menuText} ${isActive ? styles.activeText : ""}`}
+        >
+          {name}
+        </span>
+      </div>
     </div>
-  </div>
   );
 };
 
